621f3363b4d6c54d6047a05b8a0a5370e6647e78
Andrey Nazentsev <nazntrade@gmail.com>
2023-10-04 14:11:06 +0300

    first commit

---
+++ app/src/test/java/com/becker/aston_hw_5_2_navigation_navcomp_safeargs/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.becker.aston_hw_5_2_navigation_navcomp_safeargs
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aston_HW_5.2_navigation_navComp_safeArgs"
+include(":app")
+ 
